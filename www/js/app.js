eaa4df250161d412277b88736df52da07b588276
Gianni Di Noia <g@dinoia.eu>
2015-08-23 21:49:11 +0200

    test plugin

--- www/js/app.js
+++ www/js/app.js
@@ -1,20 +1,14 @@
 $(document).ready(function() {
   document.addEventListener("deviceready", onDeviceReady, false);
+  // console.log('prova');
+  onDeviceReady();
 });
-// cordova.file.dataDirectory
+
 function onDeviceReady() {
-  // console.log('ciao');
+  $('#textArea').text(cordova.file);
   // console.log(cordova.file.applicationDirectory);
-  window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "prodotti.csv", gotFile, fail);
-  // $('#textArea').text('prova');
+  // window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "prodotti.csv", gotFile, fail);
 }
-// document.addEventListener("deviceready", function() {
-//   alert("123");
-// }, true);
-// function init() {
-// window.resolveLocalFileSystemURL(cordova.file.applicationDirectory, function(f) {
-// console.dir(f);
-// }, fail);
 //This alias is a read-only pointer to the app itself
 // window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "www/index.html", gotFile, fail);
 // }
@@ -22,13 +16,16 @@ function onDeviceReady() {
 // function fail(e) {
 //   console.log("FileSystem Error");
 //   // console.dir(e);
-function gotFile(fileEntry) {
-  fileEntry.file(function(file) {
-    var reader = new FileReader();
-    reader.onloadend = function(e) {
-      console.log("Text is: " + this.result);
-      document.querySelector("#textArea").innerHTML = this.result;
-    }
-    reader.readAsText(file);
-  });
-}
+// function gotFile(fileEntry) {
+//   // fileEntry.file(function(file) {
+//   var reader = new FileReader();
+//   reader.onloadend = function(e) {
+//       console.log("Text is: " + this.result);
+//       document.querySelector("#textArea").innerHTML = this.result;
+//     }
+//     // reader.readAsText(file);
+//     // });
+// }
+// function fail(error) {
+//   console.log(error.code);
+// }
