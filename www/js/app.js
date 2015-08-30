ba6ed412a67c3675b926768986dccc1243aee822
Gianni Di Noia <g@dinoia.eu>
2015-08-30 15:45:04 +0200

    testing localstorage

--- www/js/app.js
+++ www/js/app.js
@@ -1,13 +1,39 @@
 $(document).ready(function() {
-  document.addEventListener("deviceready", onDeviceReady, false);
+  // document.addEventListener("deviceready", onDeviceReady, false);
   // console.log('prova');
   onDeviceReady();
 });
 
 function onDeviceReady() {
+  var clienti = [{
+    'nome': 'apple',
+    'città': 'cupertino',
+    'telefono': '0123456789',
+  }, {
+    'nome': 'google',
+    'città': 'mountain view',
+    'telefono': '1234567890',
+  }, ];
+  var prodotti = [{
+    'nome': 'iphone',
+    'dimensioni': 'piccolo',
+    'categoria': 'telefono',
+  }, {
+    'nome': 'nexus',
+    'dimensioni': 'medio',
+    'categoria': 'telefono',
+  }, ];
+  simpleStorage.set('clienti', clienti);
+  simpleStorage.set('prodotti', prodotti);
+  // db_set('storage', simpleStorage.storageSize());
+  // var clienti_load = simpleStorage.get(clienti);
+  // $.each(clienti_load, function(index, val) {
+  /* iterate through array or object */
+  // });
+  // for cliente in clienti_load:
   // $('#textArea').text(cordova.file);
   // console.log(cordova.file.applicationDirectory);
-  window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "prodotti.csv", gotFile, fail);
+  // window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "prodotti.csv", gotFile, fail);
 }
 //This alias is a read-only pointer to the app itself
 // window.resolveLocalFileSystemURL(cordova.file.applicationDirectory + "www/index.html", gotFile, fail);
@@ -16,16 +42,22 @@ function onDeviceReady() {
 // function fail(e) {
 //   console.log("FileSystem Error");
 //   // console.dir(e);
-function gotFile(fileEntry) {
-  fileEntry.file(function(file) {
-    var reader = new FileReader();
-    reader.onloadend = function(e) {
-      console.log("Text is: " + this.result);
-      document.querySelector("#textArea").innerHTML = this.result;
-    }
-  });
-}
-
-function fail(error) {
-  console.log(error.code);
-}
+// function gotFile(fileEntry) {
+//   fileEntry.file(function(file) {
+//     var reader = new FileReader();
+//     reader.onloadend = function(e) {
+//       console.log("Text is: " + this.result);
+//       document.querySelector("#textArea").innerHTML = this.result;
+//     }
+//   });
+// }
+// function fail(error) {
+//   console.log(error.code);
+// }
+// function db_set(key, value) {
+//   window.localStorage.setItem(key, JSON.stringify(value))
+//   return
+// }
+// function db_get(key) {
+//   return $.parseJSON(window.sessionStorage.getItem(['wage'][key]));
+// }
