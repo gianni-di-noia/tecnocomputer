08326d66e5f4d7198ce34ae4a714dff311419ed7
Gianni Di Noia <g@dinoia.eu>
2015-08-30 16:51:02 +0200

    standard message

--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,63 @@
 $(document).ready(function() {
+  var initializers = {
+    '/index.html': function() {
+      var clienti = [{
+        'nome': 'apple',
+        'città': 'cupertino',
+        'telefono': '0123456789',
+      }, {
+        'nome': 'google',
+        'città': 'mountain view',
+        'telefono': '1234567890',
+      }, ];
+      var prodotti = [{
+        'nome': 'iphone',
+        'dimensioni': 'piccolo',
+        'categoria': 'telefono',
+      }, {
+        'nome': 'nexus',
+        'dimensioni': 'medio',
+        'categoria': 'telefono',
+      }, ];
+      simpleStorage.set('clienti', clienti);
+      simpleStorage.set('prodotti', prodotti);
+    },
+    '/clienti.html': function() {
+      var clienti = simpleStorage.get('clienti');
+      console.log(clienti);
+      $.each(clienti, function(index, val) {
+        $.('#clienti_list').append('<li class="table-view-cell"> < a class = "navigate-right" href = "#" > ' + val.nome + ' < p > ' + val.città + ' < /p> < /a > < /li>');
+      });
+    },
+    '/articoli.html': function() {
+      // Do stuff for settings page
+    },
+    '/tabelle.html': function() {
+      // Do stuff for settings page
+    },
+    '/settings.html': function() {
+      // Do stuff for settings page
+    },
+  };
+
+  function initializePage(title) {
+    var func = initializers[title];
+    // Only call the initializer if it exists
+    if (func) {
+      func();
+    }
+  }
+  jQuery(document).ready(function($) {
+    // Initialize the page on push
+    window.addEventListener('push', function(e) {
+      // Remove the host from the URL to get the pathname
+      var pathname = e.detail.state.url.replace(window.location.origin, '');
+      initializePage(pathname);
+    });
+    // Initialize the page on load, using the current pathname
+    initializePage(window.location.pathname);
+  });
   // document.addEventListener("deviceready", onDeviceReady, false);
-  var clienti = [{
-    'nome': 'apple',
-    'città': 'cupertino',
-    'telefono': '0123456789',
-  }, {
-    'nome': 'google',
-    'città': 'mountain view',
-    'telefono': '1234567890',
-  }, ];
-  var prodotti = [{
-    'nome': 'iphone',
-    'dimensioni': 'piccolo',
-    'categoria': 'telefono',
-  }, {
-    'nome': 'nexus',
-    'dimensioni': 'medio',
-    'categoria': 'telefono',
-  }, ];
-  simpleStorage.set('clienti', clienti);
-  simpleStorage.set('prodotti', prodotti);
   // onDeviceReady();
   // window.addEventListener('push', myFunction);
 });
